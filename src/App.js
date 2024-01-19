7cffd88738a5fe7f0731db7336139c3a19ef7014
suburbandox <emkline@gmail.com>
2024-01-19 16:39:28 -0600

    added the css and packeges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import Papa from "papaparse";
+import food from"./foodlist2.csv"
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import Markdown from "react-markdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Food(props){
+  const food = props.food
+  return(
+    <div style={{margin: "10px",}}>
+         <Card style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src={`images/${food.Dish}.png`}
+          alt={food.Dish}
+        />
+        <Card.Body>
+          <Card.Title>{food.Dish}</Card.Title>
+        </Card.Body>
+      </Card>
     </div>
-  );
+  )
+}
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      search: "",
+      root: "",
+      spirit: "",
+    };
+  }
+  async componentDidMount() {
+    const response = await fetch(food);
+    const csvText = await response.text();
+    const parsedData = Papa.parse(csvText, { header: true }).data;
+    this.setState({ data: parsedData });
+  }
+  render(){
+    const { data ,root,spirit,search} = this.state;
+    if (data === null) {
+      return "loading...";
+      }
+    const foods = data.map((food)=>{
+      return<Food food={food} key={food.Dish} />
+    })
+    return(
+      <div>
+        {/* <h1>rrrrrrrrrrrrrr</h1>
+        <ul>
+          {data.map(cocktail => <li>{cocktail['Dish']}</li>)}
+        </ul>  */}
+        <div 
+          className="App"
+          style={{
+            display: "flex",
+            width: "80%",
+            margin: "10px auto",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {foods}
+
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
